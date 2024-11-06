5ce911e11201ce2c5a030ab8963564013304f52f
ytlee74 <ellen70733@gmail.com>
2024-11-06 22:32:40 +0800

    add about css

--- js/top.scripts.js
+++ js/top.scripts.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 
 
 
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 200) {
-    //         $('.goTopBtn').fadeIn();
-    //     } else {
-    //         $('.goTopBtn').fadeOut();           
-    //     }       
-    // })
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.goTopBtn').fadeIn();
+        } else {
+            $('.goTopBtn').fadeOut();           
+        }       
+    })
 
-    // $('.goTopBtn').click(function(){
-    //     $('html').animate({ scrollTop: 0 }, 400);
-    // })
+    $('.goTopBtn').click(function(){
+        $('html').animate({ scrollTop: 0 }, 400);
+    })
 
 
 });
