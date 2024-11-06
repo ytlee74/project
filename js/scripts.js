8257b0b46fee4fd51ca4a3fe519a10061e6db005
ytlee74 <ellen70733@gmail.com>
2024-11-06 21:42:36 +0800

    add btn top

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.goTopBtn').fadeIn();
+        } else {
+            $('.goTopBtn').fadeOut();           
+        }       
+    })
 
+    $('.goTopBtn').click(function(){
+        $('html').animate({ scrollTop: 0 }, 400);
+    })
+
+
+});
+  
